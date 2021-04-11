fb7f63396175f1d0fb2cc54a2a0746e6f17e7d39
KrGolovin <57076115+KrGolovin@users.noreply.github.com>
2021-04-12 00:04:52 +0300

    add test

--- src/main/java/ru/krgolovin/lego/mindstorm/plugin/java/task/Main.java
+++ src/main/java/ru/krgolovin/lego/mindstorm/plugin/java/task/Main.java
@@ -4,11 +4,13 @@ package ru.krgolovin.lego.mindstorm.plugin.java.task;
 import java.util.regex.Pattern;
 
 public class Main {
-    public boolean matches(String text, String regex) {
+    public static boolean matches(String text, String regex) {
         return Pattern.compile(regex).matcher(new InterimCharSequence(text, 5000L)).matches();
     }
 
     public static void main(String[] args) {
-        System.out.println(matches(" ", " "));
+        System.out.println(matches(
+                "aaaaaaaaaaaaaaaabaaaaaaaaabaaaaaaaaaaaaabaaaaaaaaaaabaaaaaaaa",
+                "(((a*)a*)a*)*b*(((a*)a*)a*)*bb "));
     }
 }
